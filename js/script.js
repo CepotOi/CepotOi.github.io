6154961ba6838cbcef9af787be841b6d09658efb
Rizki Hutama <rizkihutama69@gmail.com>
2020-08-14 17:21:52 +0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// event pada saat link di klik
+$('.page-scroll').on('click', function (e) {
+
+    // ambil is href
+    var href = $(this).attr('href');
+    // tangkap elemen yg bersangkutan
+    var elhref = $(href);
+
+    // pindahan scroll
+    $('html, body').animate({
+        scrollTop: elhref.offset().top - 163
+    }, 1100, 'easeInOutExpo');
+
+    e.preventDefault();
+
+});
+
+// parallax
+
+// about
+$(window).on('load', function () {
+    $('.pLeft').addClass('pShow');
+    $('.pRight').addClass('pShow');
+});
+
+$(window).scroll(function () {
+
+    // jumbotron
+    const wScroll = $(this).scrollTop();
+
+    $('.jumbotron img').css({
+        'transform': 'translate(0px,' + wScroll / 8 + '%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px,' + wScroll / 2 + '%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform': 'translate(0px,' + wScroll / 1.2 + '%)'
+    });
+
+    // portfolio
+    if (wScroll > $('.portfolio').offset().top - -200) {
+        $('.portfolio .card').each(function (i) {
+            setTimeout(function () {
+                $('.portfolio .card').eq(i).addClass('show');
+            }, 300 * (i + 1));
+        });
+
+    }
+});
